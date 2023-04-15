f4f2cd7a03da8bc16a77abe949956af044e37bdc
zack <yazan.mawaldi@gmail.com>
2023-04-15 17:09:46 -0400

    A lot changed here. 1) Generally added more comments for understandability 2) Added database file to remove redundancy 3) Made code cleaner to reflect my better programing skills, haha! 4) Made code much more dynamic and less hard-coded

--- main.py
+++ main.py
@@ -1,41 +1,69 @@
-import os
+import os, shutil, glob
 from redvid import Downloader
 
-import render as r
-import reddit_scraper as rs
-import uploadYT as yt
-
+import render
+import reddit_scraper
+import upload
 import config
 
-subreddit = config.subreddit  # subreddit wanted here
+
+def main():
+    # Open comma separated database. If doesn't exist, make it.
+    try:
+        database = open(config.database, 'r').read().split(',')
+    except FileNotFoundError:
+        database = ''
+        open(config.database, 'x')
+
+    # Delete temp_clips and remake it to empty the content
+    if os.path.exists("temp_clips"):
+        shutil.rmtree("temp_clips")
+    os.makedirs("temp_clips")
+
+    # Get list of videos from Reddit
+    vid_list = reddit_scraper.scrape_reddit(config.subreddit)
+
+    # Download the first video that is less than 180s and not in the database
+    for vid in vid_list:
+        reddit = Downloader(max_q=True)
+        reddit.url = vid["url"]
+        if (reddit.duration < 180) and (vid["url"] not in database):
+            print("Video chosen:")
+            for key, value in vid.items(): print(key, value)
+            reddit_scraper.download_vid(vid["url"], "temp_clips")
+            break
+
+    # Check if a suitable video was found
+    if not os.listdir("temp_clips"):
+        print("No videos less than 180s found!")
+        return False
+
+    os.rename(glob.glob(os.path.join('temp_clips', '*.mp4'))[0], os.path.join('temp_clips', 'main_clip.mp4'))
+    render.render("temp_clips", "main_clip.mp4", "output.mp4", config.video["dimensions"])
+
+    # Upload the video to YouTube
+    config.youtube["title"] = vid["title"] + " #shorts"
+    config.youtube["description"] = "video by: " + vid["author"]
+    uploaded = upload.upload("temp_clips/output.mp4", config.youtube)
+
+    # Update the database with the uploaded video's URL
+    with open(config.database, "a") as f:
+        f.write(f"{vid['url']},")
+
+    return uploaded
 
 
-login_info_text = open('reddit_login_info.txt', mode='r').read().split(',')
-directory = "temp_clips/"
-
-for file in os.listdir(directory):
-    os.remove(directory + file)
-
-vid_list = rs.reddit_scraper(subreddit)
-
-print(vid_list)
-
-x = 0
-while True:
-    if x == len(vid_list):
-        print("no vid less than 60s found!")
-        break
-    reddit = Downloader(max_q=True)
-    reddit.url = vid_list[x][0]
-    reddit.check()
-    if reddit.duration < 180:
-        rs.download_vid(vid_list[x][0], directory)
-        break
-    x += 1
-
-os.rename(directory + os.listdir(directory)[0], directory + "main_clip.mp4")
-
-r.render(directory, "main_clip.mp4", "output.mp4", config.video['dimensions'])
-
-yt.upload2YT(directory + "output.mp4", config.youtube)
-
+if __name__ == "__main__":
+    max_attempts = 10
+    success = False
+    for attempt in range(max_attempts):
+        try:
+            success = main()
+            if success:
+                print("Upload Success!")
+                break
+        except Exception as e:
+            print(f"An error occurred in main() on attempt {attempt+1}: {e}")
+    
+    if not success:
+        print(f"Attempted to upload {max_attempts} times with no luck... ending script.")

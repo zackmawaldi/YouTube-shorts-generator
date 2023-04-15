117f200bd925bc3e468f0aaa6793cbef675617ce
zack <yazan.mawaldi@gmail.com>
2023-04-15 17:04:05 -0400

    Fixed some dictionary bugs. Added more insightful comments. Added database file.

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
-subreddit = "funny"  # exclude 'r/'
+subreddit = "funny" # exclude 'r/', you can combine multiple subreddits with '+'. Ex: "funny+cars"
+database = 'database.txt' # local text database file that ensures no duplicate videos get processed.
 
-reddit_login = {  # more info to set up rovided by reddit api documentation
+# Additional setup information for authenticating with the Reddit API can be found in the Reddit API documentation.
+# Check this video: https://youtu.be/NRgfgtzIhBQ?t=50
+
+# Using this config file alone should be able to get you going without further modification.
+reddit_login = {
 	'client_id': '',
 	'client_secret': '',
 	'password': '',
@@ -10,20 +15,16 @@ reddit_login = {  # more info to set up rovided by reddit api documentation
 
 
 youtube = {
-	'title': '',
-	'description': '',
-	'tags': '',
+	'tags': ''
 	'category': 23,  # has to be an int, more about category below
-	'status': ''  # {public, private, unlisted}
+	'status': 'public'  # {public, private, unlisted}
 }
+# Note that by default, public isn't available unless you go through an audit. Checkout: https://support.google.com/youtube/contact/yt_api_form
 
 video = {
 	'dimensions': (1080, 1920),  # (horizontal, vertical), currently set vertically for tiktok
-	'blur': True  # blur non-perfect-fit clip
+	'blur': False  # blur non-perfect-fit clip
 }
 
-
-"""
-for category: has to be an int, refer to YouTube Data 4 API's documentation, but as of Jan 2022,
-checkout https://techpostplus.com/youtube-video-categories-list-faqs-and-solutions/
-"""
+# for category: has to be an int, refer to YouTube Data 4 API's documentation, but as of Jan 2022,
+# checkout https://techpostplus.com/youtube-video-categories-list-faqs-and-solutions/
